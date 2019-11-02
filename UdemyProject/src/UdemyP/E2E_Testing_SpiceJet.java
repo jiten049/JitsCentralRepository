b0c4accf3145954ec3186cd0666e2f67934d38b2
user <user@user-PC>
2019-11-02 15:52:39 +0530

    commit whole project

---
+++ UdemyProject/src/UdemyP/E2E_Testing_SpiceJet.java
@@ -0,0 +1,28 @@
+package UdemyP;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class E2E_Testing_SpiceJet {
+
+	public static void main(String[] args) {
+		
+		
+
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXTaction")).click();
+		
+		driver.findElement(By.xpath("//a[@text='Delhi (DEL)']"));
+		driver.findElement(By.xpath("(//a[@value='CCU'])[2]"));
+		
+		driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-highlight ui-state-active ui-state-hover'")).click();
+	
+		
+		
+
+	}
+
+}
